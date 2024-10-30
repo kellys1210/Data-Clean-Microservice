1f1ce8fd84cec4e3f96f9ff476e66fb617cfb79c
Kelly Shields <kellys1210@gmail.com>
2024-10-29 20:43:45 -0400

    test program finished

--- main/main.js
+++ main/main.js
@@ -10,37 +10,54 @@ app.use(cors());
 
 const router = express.Router();
 
+app.use("/", router);
+
 const filePath = "../cat_data.json";
 
-router.get("/cat-data", async (req, res) => {
+// GET method, will be called by Clean Data microservice.
+router.get("/cat-data", async (_, res) => {
   try {
+    // Opens JSON file containing data (in your program, this is where you'll make the call to your database instead)
     const data = await fs.readFile(filePath, "utf8");
+
+    // Parses received data, sends data as response to the microservice.
     const parsedData = JSON.parse(data);
-    console.log(parsedData);
-    res.status(200).json(parsedData);
+    console.log(`This is the data being sent to your microservice... ${data}`);
+    res.status(200).send(parsedData);
+
+    // Error handling
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error.message });
   }
 });
 
+// Method to handle receiving data back from microservice.
 async function getCleanCatData() {
   try {
+    // Fetches data from the local server where microservice is hosted. Once received, saves it to cleanedCatData.
     const response = await fetch("http://localhost:5004");
     const cleanedCatData = await response.json();
+
+    // Converts cleanedCatData to JSON string format.
     const stringData = JSON.stringify(cleanedCatData, null, 2);
-    console.log(stringData);
+    console.log(
+      `This is the data received back from microservice... ${stringData}`
+    );
+
+    // Error handling.
   } catch (error) {
     console.error(error);
   }
 }
 
+// Calls method to retrieve data from microservice.
 getCleanCatData();
 
-app.use("/", router);
-
+// Defines main program local server port as 5001
 const PORT = 5001;
 
+// Mounts main program onto port 5001
 app.listen(PORT, (error) => {
   if (error) {
     console.error(`Unable to connect to server: ${error.message}`);
